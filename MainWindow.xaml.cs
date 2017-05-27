dde30aa4ced0072f23c3602986402c44bb0e58f7
Czcibor <filip.ciborek@gmail.com>
2017-05-27 03:53:37 +0200

    Dodawanie haseł, zapis do pliku

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Permissions;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using PasswordKeeper;
+
 
 namespace PasswordKeeper
 {
@@ -20,9 +23,34 @@ namespace PasswordKeeper
     /// </summary>
     public partial class MainWindow : Window
     {
+        private PasswordKeeper.PasswordKepper item;
         public MainWindow()
         {
+            item = new PasswordKepper();
             InitializeComponent();
+
         }
+
+
+
+        private void AddPasswordBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+
+            item.AddPassword(UsernameTxtBox.Text,DomainTxtBox.Text,PasswordTxtBox.Text);
+            item.SavePasswords();
+            
+        }
+
+        private void GeneratePasswordBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            item.LoadPasswords();
+           
+        }
+
+        private void ViewPasswordsBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            ;
+        }
+
     }
 }
---
+++ PasswordKepper.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Media3D;
+using System.Xml.Linq;
+namespace PasswordKeeper
+{
+    public class PasswordKepper
+    {
+        public string Username = "Admin";
+        private List<Password> ListOfPasswords;
+
+
+        public PasswordKepper()
+        {
+
+            this.ListOfPasswords = new List<Password>();
+
+        }
+
+        public void AddPassword(string username, string domain, string password)
+        {
+            this.ListOfPasswords.Add(new Password(username, domain, password));
+        }
+        public void SavePasswords()
+        {
+            XDocument xml = new XDocument(
+              new XDeclaration("1.0", "utf-8", "yes"),
+              new XComment("Lista haseł"),
+              new XElement("password",
+                  from password in this.ListOfPasswords
+                  orderby password.DomainName, password.Username
+                  select new XElement("password",
+                      new XElement("username", password.Username),
+                      new XElement("domain", password.DomainName),
+                      new XElement("password", password.SecretPassword)
+                   )
+                  )
+              );
+
+            xml.Save(Username + ".xml");
+        }
+
+        public void LoadPasswords()
+        {
+            XDocument xml = XDocument.Load(Username + ".xml");
+
+            this.ListOfPasswords.Clear();
+            this.ListOfPasswords = (
+                from password in xml.Root.Elements("password")
+                select new Password(
+                    password.Element("username").Value,
+                    password.Element("domain").Value,
+                    password.Element("password").Value
+                    )
+                    ).ToList<Password>();
+            
+        }
+      
+
+    }
+   public class Password
+    {
+        public Password()
+        {
+
+        }
+
+        public Password(string username, string domainName, string secretPassword)
+        {
+            this.Username = username;
+            this.DomainName = domainName;
+            this.SecretPassword = secretPassword;
+
+        }
+
+        public string Username { get; set; }
+
+        public string DomainName { get; set; }
+
+        public string SecretPassword { get; set; }
+
+        private string SecretSeed;
+
+        private void set_SecretSeed()
+        {
+            //todo:losowanie ziarna
+            this.SecretSeed = "Abc@38d2319";
+        }
+
+    }
+}
